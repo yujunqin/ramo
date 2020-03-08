681265a0f4193e319c9419ac1c4e6ebb08ca41bc
Yue Zhang <zhngyue@umich.edu>
2020-03-07 21:13:59 -0500

    Add buff and debuff to prefab

---
+++ Assets/Scripts/BuffController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuffController : MonoBehaviour
+{
+    public enum buffType
+    {   
+        speedUp,
+        resourceUp,
+        speedDown
+    }
+
+    public buffType type;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        // respond when get touch to branch
+        if(collider.gameObject.CompareTag("branch"))
+        {
+            // publish current buff type event to player
+            
+            // TODO: get player index here
+            EventBus.Publish<BuffEvent>(new BuffEvent(type, 0));
+        }
+    }
+}
+
+public class BuffEvent
+{
+    public BuffController.buffType type;
+    public int playerIndex;
+    public BuffEvent(BuffController.buffType _type, int _playerIndex)
+    { 
+        type = _type;
+        playerIndex = _playerIndex;
+    }
+}
+
