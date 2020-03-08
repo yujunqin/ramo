3f1840e16f4f9f018b81e9e664293602187baf5c
Yue Zhang <zhngyue@umich.edu>
2020-03-08 19:08:25 -0400

    Change player speed buff

--- Assets/Scripts/BuffController.cs
+++ Assets/Scripts/BuffController.cs
@@ -12,6 +12,7 @@ public class BuffController : MonoBehaviour
     }
 
     public buffType type;
+    public int playerIndex = 1;
     
     // Start is called before the first frame update
     void Start()
@@ -33,7 +34,7 @@ public class BuffController : MonoBehaviour
             // publish current buff type event to player
             
             // TODO: get player index here
-            EventBus.Publish<BuffEvent>(new BuffEvent(type, 0));
+            EventBus.Publish<BuffEvent>(new BuffEvent(type, playerIndex, Time.time));
         }
     }
 }
@@ -42,10 +43,13 @@ public class BuffEvent
 {
     public BuffController.buffType type;
     public int playerIndex;
-    public BuffEvent(BuffController.buffType _type, int _playerIndex)
+    public float effectiveTime;
+    public float duration = 10f;
+    public BuffEvent(BuffController.buffType _type, int _playerIndex, float _effectiveTime)
     { 
         type = _type;
         playerIndex = _playerIndex;
+        effectiveTime = _effectiveTime;
     }
 }
 
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,13 +6,31 @@ public class PlayerMovement : MonoBehaviour
 {
     Rigidbody rb;
     public float MoveSpeed = 4f;
+    Subscription<BuffEvent> buffSubscription;
+    bool isSpeedingUp = false;
+    bool isSpeedingDown = false;
+    float curBuffTime = 0f;
+    float duration = 0f;
     void Start() {
         rb = GetComponent<Rigidbody>();
+        buffSubscription = EventBus.Subscribe<BuffEvent>(_OnBuffUpdated);
     }
 
     void Update() {
         Move();
         Prune();
+        if (isSpeedingUp && curBuffTime + duration > Time.time)
+        {
+            MoveSpeed = 6f;
+        }
+        else if (isSpeedingDown && curBuffTime + duration > Time.time)
+        {
+            MoveSpeed = 2f;
+        }
+        else 
+        {
+            MoveSpeed = 4f;
+        }
     }
 
     public int PlayerID = 1;
@@ -29,6 +47,49 @@ public class PlayerMovement : MonoBehaviour
         if ((PlayerID == 1 && Input.GetKeyDown(KeyCode.Q)) || (PlayerID == 2 && Input.GetKeyDown(KeyCode.Slash))){
             EventBus.Publish<PruneEvent>(new PruneEvent(PlayerID));
             Debug.Log("Player " + PlayerID.ToString() + " prunes at " + transform.position.ToString() + "!");
+        }
+    }
+
+    void _OnBuffUpdated(BuffEvent e)
+    {
+        if (e.playerIndex == PlayerID)
+        {
+            switch(e.type)
+            {
+                // make sure only one speeding buff is exist at one time
+                case BuffController.buffType.speedDown:
+                    if (isSpeedingUp) {
+                        isSpeedingUp = false;
+                        curBuffTime = 0f;
+                    }
+                    if (isSpeedingDown)
+                    {
+                        curBuffTime = e.effectiveTime;
+                        duration = e.duration;
+                    }
+                    else {
+                        isSpeedingDown = true;
+                        curBuffTime = e.effectiveTime;
+                        duration = e.duration;
+                    }
+                break;
+                case BuffController.buffType.speedUp:
+                    if (isSpeedingDown) {
+                        isSpeedingDown = false;
+                        curBuffTime = 0f;
+                    }
+                    if (isSpeedingUp)
+                    {
+                        curBuffTime = e.effectiveTime;
+                        duration = e.duration;
+                    }
+                    else {
+                        isSpeedingUp = true;
+                        curBuffTime = e.effectiveTime;
+                        duration = e.duration;
+                    }
+                break;
+            }
         }
     }
 
