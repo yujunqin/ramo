2f7912464b4d119d174452f8462755f9451288c6
Nicholas Yang <nyang2016@gmail.com>
2020-03-16 01:54:26 -0400

    flashing tooltip for root

---
+++ Assets/Scripts/TipController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TipController : MonoBehaviour
+{
+    public float timeActive;
+    public float timeBetweenFlashes;
+    private SpriteRenderer spriteRenderer;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        StartCoroutine(FlashSprite());
+        StartCoroutine(DisableGameObject());
+    }
+
+    private IEnumerator FlashSprite()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(timeBetweenFlashes);
+            spriteRenderer.enabled = !spriteRenderer.enabled;
+        }
+    }
+
+    private IEnumerator DisableGameObject()
+    {
+        yield return new WaitForSeconds(timeActive);
+        gameObject.SetActive(false);
+    }
+}
