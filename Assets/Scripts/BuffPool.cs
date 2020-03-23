bb23c95915020f5b7324fb9331a53b3bdf679d1c
Zheyu Shen <arsdragonfly@gmail.com>
2020-03-23 02:42:08 -0400

    Randomized buff generation

---
+++ Assets/Scripts/BuffPool.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuffPool : MonoBehaviour
+{
+    // TODO: merge CheckpointPool with this file
+
+    public GameObject buffPrefab;  
+    public int buffCount;
+    float Ymax = 2f;
+    float Ymin = 0f;
+    float Xmax = 7f;
+    float Xmin = 1f;
+
+    float currentY = 1f;
+    float currentX;
+    GameObject currentCheckpointL;
+    GameObject currentCheckpointR;
+    // Subscription<CheckPointEvent> checkpointSub;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Random.InitState((int)System.DateTime.Now.Ticks);
+        for (int i = 0; i < buffCount; i++)
+        {
+            currentY += Random.Range(Ymin, Ymax);
+            currentX = Random.Range(Xmin, Xmax);
+            currentCheckpointL = (GameObject)Instantiate(buffPrefab, new Vector2(-currentX, currentY) + (Vector2) transform.position, Quaternion.identity);
+            currentCheckpointR = (GameObject)Instantiate(buffPrefab, new Vector2(currentX, currentY) + (Vector2) transform.position, Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
