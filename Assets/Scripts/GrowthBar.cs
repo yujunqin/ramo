f611c0505099f7540acb82719c8f5cb77a8a6dfe
Yue Zhang <zhngyue@umich.edu>
2020-04-11 16:20:25 -0400

    Start progressbar

---
+++ Assets/Scripts/GrowthBar.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrowthBar : MonoBehaviour
+{
+    public int playerID;
+    Subscription<HeightChangeEvent> height_sub;
+    // Start is called before the first frame update
+    void Start()
+    {
+        height_sub = EventBus.Subscribe<HeightChangeEvent>(heightUpdate);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void heightUpdate(HeightChangeEvent h)
+    {
+        if (h.PlayerID != playerID)
+        {
+            return;
+        }
+        else
+        {
+            float progress = h.height / 300f;
+        }
+    }
+}
