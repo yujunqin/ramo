58a30c38f1909bfc62c11dad23e09c47616a4e06
Yue Zhang <zhngyue@umich.edu>
2020-04-19 13:42:37 -0400

    Implementing lines

--- Assets/Scripts/Pickups/GoalPointPool.cs
+++ Assets/Scripts/Pickups/GoalPointPool.cs
@@ -7,6 +7,7 @@ public class GoalPointPool : MonoBehaviour
     public GameObject goalPoint1;
     public GameObject goalPoint2;
     public GameObject goalPoint3;
+    public GameObject line;
 
     float Ymax = 3f;
     float Ymin = -3.6f;
@@ -30,6 +31,9 @@ public class GoalPointPool : MonoBehaviour
         generateCheckPoint(position1, goalPoint1);
         generateCheckPoint(position2, goalPoint2);
         generateCheckPoint(position3, goalPoint3);
+
+        generateLine(position1, position2);
+        generateLine(position2, position3);
     }
 
     // Update is called once per frame
@@ -44,6 +48,14 @@ public class GoalPointPool : MonoBehaviour
         currentCheckPointL.GetComponent<GoalPointController>().playerID = 1;
         GameObject currentCheckPointR = (GameObject)Instantiate(currentCheckPoint, new Vector2(-position.x, position.y), Quaternion.identity);
         currentCheckPointR.GetComponent<GoalPointController>().playerID = 2;
+    }
+
+    void generateLine(Vector2 position1, Vector2 position2)
+    {
+        GameObject lineL = (GameObject)Instantiate(line, position1, Quaternion.identity);
+        lineL.GetComponent<LineRenderer>().SetPosition(1, new Vector3(position2.x, position2.y, 1));
+        GameObject lineR = (GameObject)Instantiate(line, new Vector2(-position1.x, position1.y), Quaternion.identity);
+        lineR.GetComponent<LineRenderer>().SetPosition(1, new Vector3(-position2.x, position2.y, 1));
     }
 
     Vector2 generateRandomPosition(int x, int y)
